8f7bbf3a2ee70c5b4b0e7b42e8940233ae8fe615
Chris McConnell <chrimc@microsoft.com>
2020-03-06 13:16:40 -0800

    Update to explicit versioning.

--- publish.cmd
+++ publish.cmd
@@ -1,5 +1,6 @@
 echo Building and publishing %1 %2
 pushd libraries\%1
+call npm version --allow-same-verion %2
 call npm run build
 call npm publish --access public
 popd
--- publishAll.cmd
+++ publishAll.cmd
@@ -4,11 +4,8 @@ echo Must specify version
 goto done
 
 :publish
-setlocal 
-set Version=%1
-call npm run update-versions
 call publish botframework-schema %1
-call publish botbuilder-core %1
+call publish botbuilder-core %1 
 call publish adaptive-expressions %1
 call publish botbuilder-lg %1
 :done
