9034c7fbfe647708258a71d41e9042c3f5df9d22
hond <hond@microsoft.com>
2020-06-11 10:04:06 +0800

    fix comments

--- libraries/adaptive-expressions/src/expressionFunctionsForNode.ts
+++ libraries/adaptive-expressions/src/expressionFunctionsForNode.ts
@@ -1,9 +1,20 @@
+/**
+ * @module adaptive-expressions
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { ExpressionEvaluator } from './expressionEvaluator';
 import { ExpressionType } from './expressionType';
 import { ExpressionFunctions } from './expressionFunctions';
 import { ReturnType, Expression } from './expression';
 import * as os from 'os';
 
+/**
+ * Inject Node-specific functions into the common builtin-function list.
+ */
 export class ExpressionFunctionsForNode {
     public static _initialize(): void {
         this.injectedFunctions();
